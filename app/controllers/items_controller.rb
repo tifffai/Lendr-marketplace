117a6e7489951d408f5d9a31fafdc3113e27fcf1
TF <tbluebox@gmail.com>
2018-10-30 16:18:00 +1100

    Sign up form edits

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,13 +19,14 @@ class ItemsController < ApplicationController
 
   # GET /items/1/edit
   def edit
+    authorize @item
   end
 
   # POST /items
   # POST /items.json
   def create
     @item = Item.new(item_params)
-
+    @item.user = current_user
     respond_to do |format|
       if @item.save
         format.html { redirect_to @item, notice: 'Item was successfully created.' }
@@ -54,6 +55,7 @@ class ItemsController < ApplicationController
   # DELETE /items/1
   # DELETE /items/1.json
   def destroy
+    authorize @item
     @item.destroy
     respond_to do |format|
       format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
@@ -69,6 +71,7 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:name, :description, :terms, :price, :pickup_time, :street, :suburb, :state, :postcode)
+      result = params.require(:item).permit(:name, :description, :terms, :price, :pickup_time, :street, :suburb, :state, :postcode :image)
+      result[:price] = result[:price].to_f * 100.0
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/welcome'
   devise_for :users
   resources :comments
   resources :items
-  root 'items#index'
+  root 'pages#welcome'
+  get 'items', to: 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
