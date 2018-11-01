7dae102c07bdbda4fdf0f234604a9076324801bf
Linda Lai <lindalai.615@gmail.com>
2018-11-01 15:53:44 +1100

    Setup welcome page, work on user dashboard next

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   # Lendr: Added routes manually as controllers and models were generated not using scaffolding.
   root 'pages#welcome'
+  get 'dashboard', to: 'pages#dashboard'
   get 'categories', to: 'categories#index'
   get 'categories/:id', to: 'categories#show'
   get 'transactions', to: 'transactions#index'
