430dec29033149d0d2febbfeaca618cb8e61c0a2
Linda Lai <lindalai.615@gmail.com>
2018-11-05 12:01:12 +1100

    Setup redirect to categories page after successful login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :birthdate, :phone] )
     end
+
+    # Lendr: Redirects to Categories page after successful login.
+    def after_sign_in_path_for(resource)
+      categories_path
+    end
+
 end
